984637491c8899f54b1938c0734b275b0bc4a279
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-05-13 16:16:03 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import type { NextPage } from "next";
+
+const HomePage: NextPage = () => {
+  return (
+    <Typography variant="h1" color="primary">
+      Bienvenida
+    </Typography>
+  );
+};
+
+export default HomePage;
