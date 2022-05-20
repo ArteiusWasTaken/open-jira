747b88b19775ec03351c45f0ea4c6cc6497b1df1
ArteiusWasTaken <sebastiancifer@gmail.com>
2022-05-20 18:50:34 -0500

    f

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,5 @@
-import { FC, useReducer } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { FC, useEffect, useReducer } from "react";
+import { entriesApi } from "../../apis";
 
 import { Entry } from "../../interfaces";
 
@@ -13,48 +13,41 @@ interface Props {
   children?: React.ReactNode | undefined;
 }
 const Entries_INITIAL_STATE: EntriesState = {
-  entries: [
-    {
-      _id: uuidv4(),
-      description:
-        "Pendiente: Proident dolor duis elit sunt qui dolor laborum veniam ea laboris qui consequat.",
-      status: "pending",
-      createdAt: Date.now(),
-    },
-    {
-      _id: uuidv4(),
-      description:
-        "En-Progreso Veniam in cupidatat adipisicing Lorem sunt est est ex cillum laboris fugiat officia fugiat.",
-      status: "in-progress",
-      createdAt: Date.now() - 1000000,
-    },
-    {
-      _id: uuidv4(),
-      description:
-        "Terminadas: Commodo veniam aliqua tempor officia officia non laborum.",
-      status: "finished",
-      createdAt: Date.now() - 100000,
-    },
-  ],
+  entries: [],
 };
 
 export const EntriesProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      createdAt: Date.now(),
-      status: "pending",
-    };
+  const addNewEntry = async (description: string) => {
+    const { data } = await entriesApi.post<Entry>("/entries", {
+      description: description,
+    });
 
-    dispatch({ type: "[Entry] Add-Entry", payload: newEntry });
+    dispatch({ type: "[Entry] Add-Entry", payload: data });
   };
 
-  const updateEntry = (entry: Entry) => {
-    dispatch({ type: "[Entry] Entry-Updated", payload: entry });
+  const updateEntry = async ({ _id, description, status }: Entry) => {
+    try {
+      const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, {
+        description,
+        status,
+      });
+
+      dispatch({ type: "[Entry] Entry-Updated", payload: data });
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  const refreshEntries = async () => {
+    const { data } = await entriesApi.get<Entry[]>("/entries");
+    dispatch({ type: "[Entry] Refresh-data", payload: data });
+  };
+
+  useEffect(() => {
+    refreshEntries();
+  }, []);
 
   return (
     <EntriesContext.Provider
--- database/db.ts
+++ database/db.ts
@@ -21,11 +21,15 @@ export const connect = async () => {
     await mongoose.disconnect();
   }
 
-  await mongoose.connect("....");
+  await mongoose.connect(process.env.MONGO_URL || "");
   mongooConnection.isConnected = 1;
+  console.log("CONECTADO A MONGODB", process.env.MONGO_URL);
 };
 
 export const disconnect = async () => {
+  if (process.env.NODE_ENV === "development") return;
+
+  if (mongooConnection.isConnected === 0) return;
   await mongoose.disconnect();
   console.log("Desconectado de MongoDB");
 };
